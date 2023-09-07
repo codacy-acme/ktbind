e22b293cdb77b15dd6c2189eaae8d1fa42f4785f
Hélio Rocha <hjrocha@gmail.com>
2023-09-07 12:21:36 +0100

    some code

--- python/person.py
+++ python/person.py
@@ -10,7 +10,20 @@ class Person:
             return 'There is no such user'
         else:
             return self.name[user_id]
+        
+    def TowerOfHanoi(n , source, destination, auxiliary):
+        if n==1:
+            print ("Move disk 1 from source",source,"to destination",destination)
+            return
+        TowerOfHanoi(n-1, source, auxiliary, destination)
+        print ("Move disk",n,"from source",source,"to destination",destination)
+        TowerOfHanoi(n-1, auxiliary, destination, source)
 
+
+    def fibonacci_of(n):
+        if n in {0, 1}:  # Base case
+            return n
+        return fibonacci_of(n - 1) + fibonacci_of(n - 2)  # Recursive case
 
 if __name__ == '__main__':
     person = Person()
