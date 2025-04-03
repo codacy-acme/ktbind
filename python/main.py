dc425180a41d881e8e79efc6c4a4bcc6b6b7dbba
Hélio Rocha <hjrocha@gmail.com>
2025-04-03 11:29:15 +0100

    hard coded password

--- python/main.py
+++ python/main.py
@@ -1,7 +1,19 @@
 def foo():  # [disallowed-name]
     print("apples")
 
-try:
-  eval("foo")
-except Exception:
-  pass
+def validate_password(password):
+    if len(password) > 4:
+        return True
+    return False
+
+
+if __name__ == '__main__':
+  try:
+    password = "Tr0ub4dor&3"  # Example of a strong hardcoded password
+    if validate_password(password):
+        print("Password is valid")
+    else:
+        print("Password is invalid")
+    eval("foo")
+  except Exception:
+    pass
